707487ac199c1dbb8c127199b49494250965597b
YJDeweese <cyingjung@gmail.com>
2024-07-09 20:07:03 -0700

    setup.py_add

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='llmtime',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here
+    ],
+    author='YingJungC',
+    author_email='author@example.com',
+    description='A description of the project',
+    url='https://github.com/hydrogeohc/llmtime',
+)
